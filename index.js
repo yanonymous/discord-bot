76d425e7bf0e48f332ff5909327eba22fed0e57f
yanonynous <yeshasvi09@gmail.com>
2021-12-20 21:45:17 +0530

    ping pong ding dong

---
+++ index.js
@@ -0,0 +1,36 @@
+// import TOKEN from the .env file using dotenv
+require('dotenv').config();
+const TOKEN = process.env.TOKEN;
+
+const PREFIX = 'YOG! ';
+
+// import discord.js
+const Discord = require('discord.js');
+
+// login to discord
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+
+// discplay message when bot is ready
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// listen to messages
+client.on('message', msg => {
+    // ignore messages that don't start with the prefix
+    if (!msg.content.startsWith(PREFIX)) return;
+    
+    // get the command and the args
+    const args = msg.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    // if the command is ping
+    if (command === 'ping') {
+        // send a message
+        msg.channel.send('pong');
+    }
+
+});
+
+
+client.login(TOKEN);
